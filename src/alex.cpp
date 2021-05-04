8db5f89b292962ae7c391ce1c45b8af6c650c541
NickWattsCS <nsw12b@my.fsu.edu>
2021-05-04 00:27:30 -0400

    added lines starting to implement getopt

--- src/alex.cpp
+++ src/alex.cpp
@@ -1,10 +1,37 @@
 //This is the main for the program
 #include <string>
+#include <getopt.h>
 #include "lexer.h"
 #include "parser.h"
 
 int main(int argc, char** argv)
 {
+	static int file_flag;
+	static int print_to_file_flag;
+	static int verbose_flag;
+	int curflag = 0;
+	opterr = 0;
+
+	while(1)
+	{
+		static struct options long_options[] =
+		{
+			{"open",     required_argument,   &open_flag,          1},
+			{"write",    required_argument,   &write_flag, 1},
+			{"verbose",  no_argument,         &verbose_flag,       1},
+			{"file_in",  required_argument,   0,                   'f'},
+			{"more",     no_argument,         0,                   'v'},
+			{0,          0,                   0,                   0}
+		};
+		int option_index = 0;
+
+		curflag = getopt_long(argc, argv, "vf:", long_options, &option_index);
+
+		if(curflag == -1)
+			break;
+
+
+	}
 	std::string word = "";
 	std::vector<std::string> wordbank;
 	alex::lexer lang_lex;
